92529e3a5b9fd25334e9aac8a424a04e1d6eb956
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-09-06 20:46:50 -0300

    chore: add react

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,10 @@
+import * as DialogPrimitive from '@radix-ui/react-dialog'
+
+export const Dialog = () => {
+  return (
+    <DialogPrimitive.Root>
+      <DialogPrimitive.Trigger>Open</DialogPrimitive.Trigger>
+      <DialogPrimitive.Content>asdasdask</DialogPrimitive.Content>
+    </DialogPrimitive.Root>
+  )
+}
